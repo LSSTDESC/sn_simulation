3e5de0500f6fb67fbc2c850f0dd3b8d236a1e0cf
Philippe Gris <philippe.gris@clermont.in2p3.fr>
2019-05-15 08:23:46 +0200

    add sn_mafsim for pip instal

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author='Philippe Gris',
     author_email='philippe.gris@clermont.in2p3.fr',
     license='BSD',
-    packages=['sn_simulation', 'sn_simulator'],
+    packages=['sn_simulation', 'sn_simulator', 'sn_mafsim'],
     python_requires='>=3.5',
     zip_safe=False,
     install_requires=[
