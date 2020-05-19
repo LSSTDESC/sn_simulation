fb12f6b0e2335c06a5d41718158108f56e9494d4
Philippe Gris <philippe.gris@clermont.in2p3.fr>
2020-05-19 09:28:22 +0200

    setup.py update following code reorg

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author='Philippe Gris',
     author_email='philippe.gris@clermont.in2p3.fr',
     license='BSD',
-    packages=['sn_simulation', 'sn_simulator', 'sn_mafsim'],
+    packages=['sn_simulator', 'sn_wrapper'],
     python_requires='>=3.5',
     zip_safe=False,
     install_requires=[
