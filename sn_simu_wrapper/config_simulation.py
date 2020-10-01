86674b539339a9653c386314c7dc3b9578af080e
Philippe Gris <gris@clermont.in2p3.fr>
2020-10-01 14:17:31 +0200

    add scripts to read all possible simulation option from a text file and load it to a dict

---
+++ sn_simu_wrapper/config_simulation.py
@@ -0,0 +1,73 @@
+from sn_tools.sn_io import recursive_merge
+
+class ConfigSimulation:
+    def __init__(self,type_sn,model_sn,config_file):
+
+        # first: load all possible parameters
+        cdict = self.configDict(config_file)
+
+        # second: keep keys only valids for (type_sn, model_sn)
+
+        self.conf_dict = self.select(cdict,type_sn,model_sn)
+    def configDict(self,config_file):
+        """
+        Method to load a txt configuration file
+        and makes a dict out of it
+
+        Parameters
+        --------------
+        config_file: str
+          config file (txt) to transform to a dict
+
+        Returns
+        ----------
+        the dict
+
+        """
+        
+        file = open(config_file, 'r') 
+        line = file.read().splitlines()
+
+        params = {}
+        for i,ll in enumerate(line):
+            if ll!='' and ll[0]!='#':
+                lspl = ll.split(' ')
+                n = len(lspl)
+                mystr = ''
+                myclose = ''
+                for keya in [lspl[i] for i in range(n-2)]:
+                    mystr += '{\''+keya+ '\':'
+                    myclose +=' }'
+
+                if lspl[n-1]!= 'str':
+                    dd = '{} {} {}'.format(mystr,eval('{}({})'.format(lspl[n-1],lspl[n-2])),myclose)
+                else:
+                   dd = '{} \'{}\' {}'.format(mystr,lspl[n-2],myclose)
+
+                thedict = eval(dd)
+                params = recursive_merge(params, thedict)
+
+        return params
+
+    def select(self, thedict, type_sn, model_sn):
+        """
+        Method to select thedict keys according to type_sn and model_sn
+
+        Parameters
+        ---------------
+        thedict: dict
+          original dict
+        type_sn: str
+          SN type
+        model_sn: str
+          SN model for simulation
+        """
+
+        res = dict(thedict)
+        if 'Ia' not in type_sn:
+            del res['SN']['x1']
+            del res['SN']['color']
+            del res['SN']['x1_color']
+
+        return res
+        
