e0034b2113dd9756a8a921795859605c5733309a
Philippe Gris <philippe.gris@clermont.in2p3.fr>
2020-10-16 13:36:08 +0200

    modified setup.py to install txt files in sn_simu_input

--- setup.py
+++ setup.py
@@ -1,20 +1,22 @@
 from setuptools import setup
 
 # get the version here
-pkg_vars  = {}
+pkg_vars = {}
 
 with open("version.py") as fp:
     exec(fp.read(), pkg_vars)
 
 setup(
     name='sn_simulation',
-    version= pkg_vars['__version__'],
+    version=pkg_vars['__version__'],
     description='Simulations for supernovae',
     url='http://github.com/lsstdesc/sn_simulation',
     author='Philippe Gris',
     author_email='philippe.gris@clermont.in2p3.fr',
     license='BSD',
-    packages=['sn_simulator', 'sn_simu_wrapper','sn_simu_input'],
+    packages=['sn_simulator', 'sn_simu_wrapper', 'sn_simu_input'],
+    # All files from folder sn_simu_input
+    package_data={'sn_simu_input': ['*.txt']},
     python_requires='>=3.5',
     zip_safe=False,
     install_requires=[
